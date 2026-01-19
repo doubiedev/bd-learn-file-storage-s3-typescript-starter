db9bea9d851585826cf42cbbe3c9e6ee95c374c5
Nicholas Doublet <thedoubiedev@gmail.com>
2026-01-19 14:25:02 +1100

    match 6.6 solution

--- src/api/video-meta.ts
+++ src/api/video-meta.ts
@@ -51,12 +51,14 @@ export async function handlerVideoGet(cfg: ApiConfig, req: BunRequest) {
         throw new BadRequestError("Invalid video ID");
     }
 
-    const video = getVideo(cfg.db, videoId);
+    let video = getVideo(cfg.db, videoId);
     if (!video) {
         throw new NotFoundError("Couldn't find video");
     }
 
-    return respondWithJSON(200, dbVideoToSignedVideo(cfg, video));
+    video = await dbVideoToSignedVideo(cfg, video);
+
+    return respondWithJSON(200, video);
 }
 
 export async function handlerVideosRetrieve(cfg: ApiConfig, req: Request) {
@@ -64,6 +66,10 @@ export async function handlerVideosRetrieve(cfg: ApiConfig, req: Request) {
     const userID = validateJWT(token, cfg.jwtSecret);
 
     const videos = getVideos(cfg.db, userID);
-    videos.forEach(video => dbVideoToSignedVideo(cfg, video));
-    return respondWithJSON(200, videos);
+
+    const signedVideos = await Promise.all(
+        videos.map((video) => dbVideoToSignedVideo(cfg, video)),
+    );
+
+    return respondWithJSON(200, signedVideos);
 }
--- src/api/videos.ts
+++ src/api/videos.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { getBearerToken, validateJWT } from "../auth";
 import { getVideo, updateVideo, type Video } from "../db/videos";
 import { respondWithJSON } from "./json";
-import { uploadVideoToS3 } from "../s3";
+import { uploadVideoToS3, generatePresignedURL } from "../s3";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 import { type ApiConfig } from "../config";
@@ -49,14 +49,16 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
     const key = `${aspectRatio}/${videoId}.mp4`;
     await uploadVideoToS3(cfg, key, processedFilePath, "video/mp4");
 
-    video.videoURL = key;
+    video.videoURL = `${key}`;
     updateVideo(cfg.db, video);
 
     await Promise.all([
         rm(tempFilePath, { force: true }),
         rm(`${tempFilePath}.processed.mp4`, { force: true }),
     ]);
-    return respondWithJSON(200, dbVideoToSignedVideo(cfg, video));
+
+    const signedVideo = dbVideoToSignedVideo(cfg, video);
+    return respondWithJSON(200, signedVideo);
 }
 
 export async function getVideoAspectRatio(filePath: string) {
@@ -133,22 +135,12 @@ export async function processVideoForFastStart(inputFilePath: string) {
     return processedFilePath;
 }
 
-export function generatePresignedURL(cfg: ApiConfig, key: string, expireTime: number) {
-    const presignedURL = cfg.s3Client.presign(key, {
-        expiresIn: expireTime,
-    });
-
-    return presignedURL;
-}
-
-export function dbVideoToSignedVideo(cfg: ApiConfig, video: Video) {
-    const videoURL = video.videoURL;
-    if (!videoURL) {
-        throw new NotFoundError("Couldn't find video URL");
+export async function dbVideoToSignedVideo(cfg: ApiConfig, video: Video) {
+    if (!video.videoURL) {
+        return video;
     }
 
-    const presignedURL = generatePresignedURL(cfg, videoURL, 60);
-    video.videoURL = presignedURL;
+    video.videoURL = await generatePresignedURL(cfg, video.videoURL, 5 * 60);
 
     return video;
 }
